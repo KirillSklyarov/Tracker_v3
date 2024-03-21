3c560a3f4c8ab3b43762b6ff84f5a3e601761fac
Kirill_Sklyarov <k.sklyarov@yandex.ru>
2024-03-21 12:44:17 +0300

    - исправления 1.03. - Исправлен tapGesture.

--- Tracker/Extensions/UIViewController+Ext.swift
+++ Tracker/Extensions/UIViewController+Ext.swift
@@ -12,6 +12,7 @@ extension UIViewController {
     
     func addTapGestureToHideKeyboard() {
         let tapGesture = UITapGestureRecognizer(target: view, action: #selector(view.endEditing))
+        tapGesture.cancelsTouchesInView = false
         view.addGestureRecognizer(tapGesture)
     }
 }
